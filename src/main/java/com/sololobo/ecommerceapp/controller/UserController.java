fbf0fe8269a035f56eb99e6c301dabd7d0e5e38f
sololobo <smshaon02@yahoo.com>
2021-11-19 23:40:20 +0800

    post mapping and get mapping working fine

--- src/main/java/com/sololobo/ecommerceapp/controller/UserController.java
+++ src/main/java/com/sololobo/ecommerceapp/controller/UserController.java
@@ -3,14 +3,29 @@ package com.sololobo.ecommerceapp.controller;
 import com.sololobo.ecommerceapp.domain.User;
 import com.sololobo.ecommerceapp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 
 
 @RestController
 public class UserController {
-
-
     @Autowired
     UserRepository userRepository;
+
+    @GetMapping("/users")
+    public List<User> getUser(){
+        return userRepository.findAll();
+    }
+
+    @PostMapping
+    public void postUser(@RequestBody User user){
+        //System.out.println(user);
+        userRepository.save(user);
+    }
+
+
 }
