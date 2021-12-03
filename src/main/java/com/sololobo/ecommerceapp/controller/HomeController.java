d1331f9e57da8a2b72ee653a535b6d22cb7910a7
sololobo <smshaon02@yahoo.com>
2021-12-03 16:03:18 +0800

    created HomeController

---
+++ src/main/java/com/sololobo/ecommerceapp/controller/HomeController.java
@@ -0,0 +1,25 @@
+package com.sololobo.ecommerceapp.controller;
+
+import com.sololobo.ecommerceapp.domain.Product;
+import com.sololobo.ecommerceapp.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+    @Autowired
+    ProductRepository productRepository;
+
+    @GetMapping("/homeBase")
+    public ModelAndView homePage(){
+        List<Product> productList = productRepository.findAll();
+
+
+        return new ModelAndView("homeBase")
+                .addObject("productList", productList);
+    }
+}
