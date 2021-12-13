f8abd01e085ff640f82dcba6fb8799c04edc1f4d
sololobo <smshaon02@yahoo.com>
2021-12-14 00:19:00 +0800

    customize the enum properties with url and image url

--- src/main/java/com/sololobo/ecommerceapp/domain/enumeration/ProductCategory.java
+++ src/main/java/com/sololobo/ecommerceapp/domain/enumeration/ProductCategory.java
@@ -1,7 +1,23 @@
 package com.sololobo.ecommerceapp.domain.enumeration;
 
 public enum ProductCategory {
-    CAR,
-    BIKE,
-    BICYCLE
+    CAR("/car", "https://www.exoticcarlist.com/blog/wp-content/uploads/2017/02/5636b9710ab58f6455a19a931a7631b7_bugatti-veyron-super-sport-bugatti-veyron-super-sport-clipart_1600-1060.jpeg"),
+    BIKE("/bike", "https://silodrome.com/wp-content/uploads/2015/03/BMW-Motorcycle-1-1600x1000.jpg"),
+    BICYCLE("/bicycle", "https://images.singletracks.com/blog/wp-content/uploads/2016/05/20160506_135551-1200x675.jpg");
+
+    private final String url;
+    private final String imgUrl;
+
+    ProductCategory(String url, String imgUrl) {
+        this.url = url;
+        this.imgUrl = imgUrl;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public String getUrl() {
+        return url;
+    }
 }
