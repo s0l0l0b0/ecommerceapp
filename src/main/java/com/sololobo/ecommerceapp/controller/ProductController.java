c73a4521d7d74969b294c5fc549065fc379e70ba
sololobo <smshaon02@yahoo.com>
2021-12-14 00:20:37 +0800

    created a @GetMapping for the car view

--- src/main/java/com/sololobo/ecommerceapp/controller/ProductController.java
+++ src/main/java/com/sololobo/ecommerceapp/controller/ProductController.java
@@ -1,35 +1,24 @@
 package com.sololobo.ecommerceapp.controller;
 
 import com.sololobo.ecommerceapp.domain.Product;
-import com.sololobo.ecommerceapp.domain.Supplier;
 import com.sololobo.ecommerceapp.repository.ProductRepository;
-import com.sololobo.ecommerceapp.repository.SupplierRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class ProductController {
+
     @Autowired
     ProductRepository productRepository;
 
-    @PostMapping("/products")
-    public void postProductr(@RequestBody Product product){
-        //System.out.println(user);
-        productRepository.save(product);
-    }
+    @GetMapping("/car")
+    public ModelAndView returnCar(){
+        List<Product> carList = productRepository.findAll();
 
-    @GetMapping("/products")
-    public void getProducts(){
-        //return productRepository.findAll();
-        List<Product> productList = productRepository.findAll();
-        for(int i = 0; i < productList.size(); i++){
-            System.out.println(productList.get(i));
-        }
-
+        return new ModelAndView("car");
     }
 }
