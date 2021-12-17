6177af61f580fd039f9ddbfbf02226979d92c525
sololobo <smshaon02@yahoo.com>
2021-12-17 20:20:38 +0800

    a query to get product category

--- src/main/java/com/sololobo/ecommerceapp/repository/ProductRepository.java
+++ src/main/java/com/sololobo/ecommerceapp/repository/ProductRepository.java
@@ -1,7 +1,18 @@
 package com.sololobo.ecommerceapp.repository;
 
 import com.sololobo.ecommerceapp.domain.Product;
+import com.sololobo.ecommerceapp.domain.enumeration.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+//    @Query("SELECT c from Product c where c.productCategory = :category")
+//    List<Product> getAllByCategory(ProductCategory category);
+
+    List<Product> getByProductCategory(ProductCategory category);
+
 }
