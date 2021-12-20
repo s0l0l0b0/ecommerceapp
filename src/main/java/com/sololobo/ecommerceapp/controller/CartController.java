460cdba2faa0560b7f448ec437a0a9bc98d020b0
sololobo <smshaon02@yahoo.com>
2021-12-20 23:27:44 +0800

    exception handle successful

--- src/main/java/com/sololobo/ecommerceapp/controller/CartController.java
+++ src/main/java/com/sololobo/ecommerceapp/controller/CartController.java
@@ -1,21 +1,24 @@
 package com.sololobo.ecommerceapp.controller;
 
 import com.sololobo.ecommerceapp.domain.redis.Cart;
+import com.sololobo.ecommerceapp.repository.ProductRepository;
 import com.sololobo.ecommerceapp.repository.redis.CartRepository;
 import com.sololobo.ecommerceapp.utility.Utility;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Optional;
 
-@RestController
+@Controller
 public class CartController {
     @Autowired
     CartRepository cartRepository;
+    @Autowired
+    ProductRepository productRepository;
 
+    @ResponseBody
     @PostMapping("/add-to-cart")
     public void addToCart(@RequestParam Long productId){
         String userEmail = Utility.getLoggedInUserEmail();
@@ -28,10 +31,15 @@ public class CartController {
         cartRepository.save(cart);
     }
 
-    @GetMapping("add-to-cart")
-    public void testCart(){
-        String userEmail = "nice@nice.com";
+    @GetMapping("/cart")
+    public ModelAndView cart(){
+        String userEmail = Utility.getLoggedInUserEmail();
         Optional<Cart> byId = cartRepository.findById(userEmail);
-        System.out.println(byId);
+        if(byId.isEmpty()){
+            throw new IllegalArgumentException("cart not found");
+        }
+        return new ModelAndView("cart")
+                .addObject("products", productRepository.getByIds(byId.get().getProductIds()));
+
     }
 }
