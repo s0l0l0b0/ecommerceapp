027ce64c94c672111e8dac13f947517f7301e411
sololobo <smshaon02@yahoo.com>
2021-12-20 23:28:27 +0800

    added a query to find products by id

--- src/main/java/com/sololobo/ecommerceapp/repository/ProductRepository.java
+++ src/main/java/com/sololobo/ecommerceapp/repository/ProductRepository.java
@@ -7,11 +7,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Set;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 //    @Query("SELECT c from Product c where c.productCategory = :category")
 //    List<Product> getAllByCategory(ProductCategory category);
+    @Query("SELECT p from Product p where p.id in :productIds")
+    List<Product> getByIds (Set<Long> productIds);
 
     List<Product> getByProductCategory(ProductCategory category);
 
